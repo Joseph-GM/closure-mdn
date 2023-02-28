dcd508dbfc996be26e40b6ddd64a573b0cfeb097
Joseph-GM <jkim.english75@gmail.com>
2023-02-28 14:37:36 +0900

    2023-02-28

---
+++ common-mistake/javascript1.js
@@ -0,0 +1,26 @@
+function showHelp(help) {
+    document.getElementById("help").textContent = help;
+  }
+  
+  function makeHelpCallback(help) {
+    return function () {
+        console.log(help);
+      showHelp(help);
+    };
+  }
+  
+  function setupHelp() {
+    var helpText = [
+      { id: "email", help: "Your email address" },
+      { id: "name", help: "Your full name" },
+      { id: "age", help: "Your age (you must be over 16)" },
+    ];
+  
+    for (var i = 0; i < helpText.length; i++) {
+      var item = helpText[i];
+      document.getElementById(item.id).onfocus = makeHelpCallback(item.help);
+    }
+  }
+  
+  setupHelp();
+  
